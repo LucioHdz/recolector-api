50820b0384bfc3a50b22398946a39b9d549d954d
LucioHdz <luciohdz0@gmail.com>
2023-05-21 13:46:36 -0600

    arreglando post de pedidios

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -44,6 +44,30 @@ pedidos.get('/', (req, res) => {
         }
     })
 })
+pedidos.get('/:id', (req, res) => {
+    const {id} = req.params
+    db.query(`SELECT
+    pedidos.id_pedido,
+    usuarios.nombre_usuario,
+    productos.nombre_producto,
+    pedidos.cantidad,
+    pedidos.total,
+    pedidos.fecha_pedido,
+    transportes.nombre_transporte,
+    pedidos.no_contacto
+    FROM
+    pedidos
+    INNER JOIN usuarios ON pedidos.id_usuario = usuarios.id_usuario
+    INNER JOIN productos ON pedidos.id_producto = productos.id_producto
+    INNER JOIN transportes ON pedidos.id_transporte = transportes.id_transporte where usuarios.id_usuario = ${id}`, (err, result) => {
+        if (err) {
+            res.status(500).json({ message: err, message })
+        } else {
+            res.status(200).json(result)
+        }
+    })
+
+})
 
 
 
